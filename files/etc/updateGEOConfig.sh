c8663f714ea2675beef1447ac9c50b35ab9d6af1
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-01-23 12:24:11 +0800

    optimize GEO assets update

--- files/etc/updateGEOConfig.sh
+++ files/etc/updateGEOConfig.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ## v2ray geosite
-curl -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -k
+curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
 # v2ray geoip
-curl -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -k
+curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
 # xray geosite
-curl -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -k
+curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
 # xray geoip
-curl -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -k
+curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
 # 重启程序以触发更新 ipset
 # /etc/init.d/passwall reload
