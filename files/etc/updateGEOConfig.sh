d9effeca0184c1da8b464b6ef8aaa66d8ea9e815
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-01-24 13:19:43 +0800

    Update updateGEOConfig.sh
    ensure success download by roughly measure

--- files/etc/updateGEOConfig.sh
+++ files/etc/updateGEOConfig.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
-## v2ray geosite
-curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+# v2ray geosite
+# main
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
+# back
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+# pause a while
+sleep 5
 # v2ray geoip
-curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+# main
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
+# back
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+# pause a while
+sleep 5
 # xray geosite
-curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+# main
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
+# back
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+# pause a while
+sleep 5
 # xray geoip
-curl -L --retry 10 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+# main
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
+# back
+curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+# pause a while
+sleep 5
 # 重启程序以触发更新 ipset
 # /etc/init.d/passwall reload
