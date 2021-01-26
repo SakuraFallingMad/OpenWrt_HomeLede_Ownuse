08f7194ca4f76e01edc8db9941d980b700a69d22
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-01-26 10:24:48 +0800

    add connection timeout

--- files/etc/updateGEOConfig.sh
+++ files/etc/updateGEOConfig.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 # v2ray geosite
 # main
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
 # back
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
 # pause a while
 sleep 5
 # v2ray geoip
 # main
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
 # back
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/v2ray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
 # pause a while
 sleep 5
 # xray geosite
 # main
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat" -S
 # back
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geosite.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" -S
 # pause a while
 sleep 5
 # xray geoip
 # main
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat" -S
 # back
-curl -L --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
+curl -L --connect-timeout 20 --retry 5 --retry-all-errors --retry-delay 5 -o /usr/share/xray/geoip.dat "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" -S
 # pause a while
 sleep 5
 # 重启程序以触发更新 ipset
