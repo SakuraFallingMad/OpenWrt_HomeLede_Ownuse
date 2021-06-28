b1204ad27e4b6ddb1cc6f514ce66cd9f72116205
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-06-28 18:02:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -39,7 +39,6 @@ disablePkgsList="
 ./package/lean/luci-app-softethervpn 
 ./package/lean/luci-app-netdata 
 ./feeds/packages/utils/docker-compose 
-./feeds/packages/net/adguardhome
 ./feeds/packages/net/miniupnpd 
 ./feeds/packages/net/mwan3 
 ./feeds/packages/utils/dockerd 
@@ -48,6 +47,7 @@ disablePkgsList="
 ./feeds/packages/utils/libnetwork 
 ./feeds/packages/utils/tini 
 ./feeds/packages/utils/runc 
+./feeds/packages/net/adguardhome 
 ./feeds/packages/net/dnscrypt-proxy2 
 ./feeds/packages/net/https-dns-proxy 
 ./feeds/packages/net/kcptun 
