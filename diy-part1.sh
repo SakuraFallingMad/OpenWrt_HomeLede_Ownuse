c3e93ebe9b66d429ebde044edb4a92b4d2b03d02
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-10-18 17:51:23 +0800

    disable luci-app-vssr

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add jerrykuku's vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/lean/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/lean/luci-app-vssr
 
 # Add HomeLede prepareCompile.sh
 disablePkgsList="
