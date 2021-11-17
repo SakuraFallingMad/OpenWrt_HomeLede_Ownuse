909e1d60143e82f1ccec3faa8fe52e8c9901c9d0
SakuraFallingMad <31793080+SakuraFallingMad@users.noreply.github.com>
2021-11-17 19:24:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -46,6 +46,10 @@ disablePkgsList="
 ./feeds/packages/utils/runc 
 ./feeds/packages/utils/tini 
 ./feeds/pw/chinadns-ng 
+./feeds/pw/dns2socks/
+./feeds/pw/ipt2socks/
+./feeds/pw/microsocks/
+./feeds/pw/pdnsd-alt/
 ./feeds/xiaoqingfeng/softethervpn5 
 "
 
